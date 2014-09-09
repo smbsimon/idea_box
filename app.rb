b9718040f24d7aec324154e92b3b885c86f813e2
Sara Simon <smbsimon@gmail.com>
2014-09-09 17:39:23 -0600

    deleting ideas

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'bundler'
 Bundler.require
 
 class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
   configure :development do
     register Sinatra::Reloader
   end
@@ -18,6 +20,11 @@ class IdeaBoxApp < Sinatra::Base
   post '/' do
     idea = Idea.new(params['idea_title'], params['idea_description'])
     idea.save
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
     redirect '/'
   end
 end
--- idea.rb
+++ idea.rb
@@ -27,6 +27,12 @@ class Idea
     end
   end
 
+  def self.delete(position)
+    database.transaction do
+      database['ideas'].delete_at(position)
+    end
+  end
+
   def self.database
     @database ||= YAML::Store.new "ideabox"
   end
