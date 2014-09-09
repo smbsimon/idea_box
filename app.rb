36202d5abc00cf0716ae5a39ebe4101e2725d74a
Sara Simon <smbsimon@gmail.com>
2014-09-09 15:01:11 -0600

    recording ideas

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require './idea'
 require 'bundler'
 Bundler.require
 
@@ -8,5 +9,20 @@ class IdeaBoxApp < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  not_found do
+    erb :error
+  end
+
+  post '/' do
+  # 1. Create an idea based on the form parameters
+  idea = Idea.new
+
+  # 2. Store it
+  idea.save
+
+  # 3. Send us back to the index page to see all ideas
+  "Creating an idea!"
   end
 end
