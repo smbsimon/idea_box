9978c59dc82697a0055136f2cc18cede90df9924
Sara Simon <smbsimon@gmail.com>
2014-09-09 16:43:31 -0600

    saving ideas

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   get '/' do
-    erb :index
+    erb :index, locals: {ideas: Idea.all}
   end
 
   not_found do
@@ -16,13 +16,8 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   post '/' do
-  # 1. Create an idea based on the form parameters
-  idea = Idea.new
-
-  # 2. Store it
-  idea.save
-
-  # 3. Send us back to the index page to see all ideas
-  "Creating an idea!"
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save
+    redirect '/'
   end
 end
