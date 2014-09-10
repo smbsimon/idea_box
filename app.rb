283dc4d9b3ff5fea95f663a1acc0c94811ff392f
Sara Simon <smbsimon@gmail.com>
2014-09-10 10:04:47 -0600

    moved class methods to ideastore

--- app.rb
+++ app.rb
@@ -1,40 +1,40 @@
 require './idea'
-require 'bundler'
-Bundler.require
+require './idea_store'
+# require 'bundler'
+# Bundler.require
 
 class IdeaBoxApp < Sinatra::Base
   set :method_override, true
 
-  configure :development do
-    register Sinatra::Reloader
-  end
-
-  get '/' do
-    erb :index, locals: {ideas: Idea.all}
-  end
+  # configure :development do
+  #   register Sinatra::Reloader
+  # end
 
   not_found do
     erb :error
   end
 
+  get '/' do
+    erb :index, locals: {ideas: IdeaStore.all, idea: Idea.new(params)}
+  end
+
   post '/' do
-    idea = Idea.new(params[:idea])
-    idea.save
+    IdeaStore.create(params[:idea])
     redirect '/'
   end
 
   delete '/:id' do |id|
-    Idea.delete(id.to_i)
+    IdeaStore.delete(id.to_i)
     redirect '/'
   end
 
   get '/:id/edit' do |id|
-    idea = Idea.find(id.to_i)
+    idea = IdeaStore.find(id.to_i)
     erb :edit, locals: {id: id, idea: idea}
   end
 
   put '/:id' do |id|
-    Idea.update(id.to_i, params[:idea])
+    IdeaStore.update(id.to_i, params[:idea])
     redirect '/'
   end
 end
--- idea.rb
+++ idea.rb
@@ -1,5 +1,3 @@
-require 'yaml/store'
-
 class Idea
   attr_reader :title, :description
 
@@ -17,43 +15,5 @@ class Idea
 
   def database
     Idea.database
-  end
-
-  def self.database
-    @database ||= YAML::Store.new "ideabox"
-  end
-
-  def self.all
-    raw_ideas.map do |data|
-      Idea.new(data)
-    end
-  end
-
-  def self.raw_ideas
-    database.transaction do |db|
-      db['ideas'] || []
-    end
-  end
-
-  def self.delete(position)
-    database.transaction do
-      database['ideas'].delete_at(position)
-    end
-  end
-
-  def self.find(id)
-    Idea.new(find_raw_idea(id))
-  end
-
-  def self.find_raw_idea(id)
-    database.transaction do
-      database['ideas'].at(id)
-    end
-  end
-
-  def self.update(id, data)
-    database.transaction do
-      database['ideas'][id] = data
-    end
   end
 end
