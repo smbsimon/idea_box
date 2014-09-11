33cd681d32ffe279902eb64226ce11dabc01d6ec
Josh Cheek <josh.cheek@gmail.com>
2014-09-11 15:14:50 -0600

    Remove unused methods from Idea

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -9,20 +9,12 @@ class Idea
     @id          = attributes["id"]
   end
 
-  def save
-    IdeaStore.create(to_h)
-  end
-
   def to_h
     {
       "title"       => title,
       "description" => description,
       "rank"        => rank
     }
-  end
-
-  def database
-    Idea.database
   end
 
   def like!
