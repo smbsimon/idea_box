e9dca6dfe06028bbbbcab5ba81d3223f5ca127d0
Josh Cheek <josh.cheek@gmail.com>
2014-09-11 15:36:31 -0600

    Reorganize and add some tests

--- test/app_test.rb
+++ test/app_test.rb
@@ -15,12 +15,24 @@ describe IdeaBoxApp do
     IdeaBoxApp.new
   end
 
-  it "provides user a place to record ideas" do
-    get '/'
-    assert_equal 200, last_response.status
-    # assert the form is on the page,
-    # and the names look like the data that I'm posting
-    assert last_response.ok?
+  describe 'homepage' do
+    it "provides user a place to record ideas" do
+      get '/'
+      assert_equal 200, last_response.status
+      # assert the form is on the page,
+      # and the names look like the data that I'm posting
+      assert last_response.ok?
+    end
+
+    it "stores ideas and descriptions after entered on form" do
+      post '/', idea: { title: "test-idea",
+                        description: "test-description" }
+      follow_redirect!
+      assert last_response.ok?
+      assert last_response.body.include? "test-idea"
+    end
+
+    it 'shows the task list in order of priority'
   end
 
   it "doesn't find routes that don't exist" do
@@ -28,11 +40,14 @@ describe IdeaBoxApp do
     assert_equal 404, last_response.status
   end
 
-  it "stores ideas and descriptions after entered on form" do
-    post '/', idea: { title: "test-idea",
-                      description: "test-description" }
-    follow_redirect!
-    assert last_response.ok?
-    assert last_response.body.include? "test-idea"
+  it 'can bump the priority of the tasks' do
+    # put two tasks into the db
+    # go to the homepage
+    # bump the bottom one
+    # go to the homepage
+    # the bumped one should be on top
   end
+
+  it 'can edit tasks'
+  it 'can delete tasks'
 end
