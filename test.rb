ddbb0cdf287cbbb5552e6a6fd6d18fa1b64c82ac
Sara Simon <smbsimon@gmail.com>
2014-09-10 19:09:20 -0600

    added css stylesheet

--- test.rb
+++ test.rb
@@ -13,9 +13,21 @@ describe IdeaBoxApp do
     IdeaBoxApp.new
   end
 
-  it "provides you a place to record ideas" do
+  it "provides user a place to record ideas" do
     get '/'
     assert_equal 200, last_response.status
     assert last_response.ok?
+  end
+
+  it "doesn't find routes that don't exist" do
+    get '/this-route-does-not-exist'
+    assert_equal 404, last_response.status
+  end
+
+  it "stores ideas and descriptions after entered on form" do
+    post '/', idea: { title: "test-idea", description: "test-description" }
+    follow_redirect!
+    assert last_response.ok?
+    assert last_response.body.include? "test-idea"
   end
 end
